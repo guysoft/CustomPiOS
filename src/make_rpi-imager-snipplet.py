8837c7403809cce955eeb12632c09478501d300a
Guy Sheffer <guysoft@gmail.com>
2022-01-27 00:28:20 +0200

    Add script to generate snipplet for rpi-imager, and add its values to the example distro  #151

--- src/make_rpi-imager-snipplet.py
+++ src/make_rpi-imager-snipplet.py
@@ -8,6 +8,15 @@ from datetime import date
 import glob
 
 
+
+def handle_arg(key):
+    if key in os.environ.keys():
+        return os.environ[key]
+    else:
+        print("Error: Missing value in your distro config file for rpi-imager json generator: " + str(key))
+        exit(1)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=True, description='Create a json snipplet from an image to be used with the make_rpi-imager_list.py and eventually published in a repo')
     parser.add_argument('workspace_suffix', nargs='?', default="default", type=str, help='Suffix of workspace folder')
@@ -19,17 +28,17 @@ if __name__ == "__main__":
     if args.workspace_suffix != "" and args.workspace_suffix != "default":
         workspace_path += "-" + args.workspace_suffix
     
-    name = os.environ["RPI_IMAGER_NAME"]
-    description = os.environ["RPI_IMAGER_DESCRIPTION"]
+    name = handle_arg("RPI_IMAGER_NAME")
+    description = handle_arg("RPI_IMAGER_DESCRIPTION")
     url = args.rpi_imager_url
-    icon = os.environ["RPI_IMAGER_ICON"]
+    icon = handle_arg("RPI_IMAGER_ICON")
     release_date = date.today().strftime("%Y-%m-%d")
     zip_local = glob.glob(os.path.join(workspace_path,"*.zip"))[0]
-    img_sha256_path = glob.glob(os.path.join(workspace_path,"*.img.sha256"))[0]
-    img_sha256 = ""
-    with open(img_sha256_path, 'r') as f:
-        img_sha256 = f.read().split()[0]
+    
+    if url == "MISSING_URL":
+        url = os.path.basename(zip_local)
         
+
     output_path = os.path.join(workspace_path, "rpi-imager-snipplet.json")
     
     json_out = {"name": name,
@@ -38,14 +47,19 @@ if __name__ == "__main__":
                 "icon": icon,
                 "release_date": release_date,
                 }
+
+    img_sha256_path = glob.glob(os.path.join(workspace_path,"*.img.sha256"))[0]
+    json_out["extract_sha256"] = None
+    with open(img_sha256_path, 'r') as f:
+        json_out["extract_sha256"] = f.read().split()[0]
+
     json_out["extract_size"] = None
     with zipfile.ZipFile(zip_local) as zipfile:
         json_out["extract_size"] = zipfile.filelist[0].file_size
     
-    json_out["extract_sha256"] = None
     json_out["image_download_size"] = os.stat(zip_local).st_size
-    json_out["image_download_sha256"] = img_sha256
-    
+
+    json_out["image_download_sha256"] = None
     with open(zip_local,"rb") as f:
         json_out["image_download_sha256"] = hashlib.sha256(f.read()).hexdigest()
     
