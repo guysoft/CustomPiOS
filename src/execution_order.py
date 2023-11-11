c706025808b21db221460b7f0699c7e88a1cf9f7
Guy Sheffer <guysoft@gmail.com>
2023-11-11 23:23:39 +0200

    Inital support for meta modules

--- src/execution_order.py
+++ src/execution_order.py
@@ -1,31 +1,41 @@
 #a='base(octopi,a(b,c(a2)),mm)'
 import argparse
 import os
+import subprocess
 from get_remote_modules import get_remote_module
+from typing import TextIO, List, Tuple, Dict, Any
 
-def handle(module, state, out):
+
+def run_command(command: List[str], **kwargs: Dict[str, Any]):
+    is_timeout = False
+    p = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
+    try:
+        stdout, stderr = p.communicate(timeout=5)
+    except subprocess.TimeoutExpired as e:
+        p.kill()
+        stdout,stderr = p.communicate()
+        is_timeout = True
+    try:
+        stdout = stdout.decode("utf-8")
+    except UnicodeDecodeError as e:
+        print("Error: can't decode stdout")
+        print(e)
+        print(stdout)
+        stdout = ""
+
+    try:
+        stderr = stderr.decode("utf-8")
+    except UnicodeDecodeError as e:
+        print("Error: can't decode stderr")
+        print(stderr)
+        print(e)
+        stderr = ""
+
+    return stdout, stderr, is_timeout
+
+    
+def write_modules_scripts(module: str, state: str, module_folder: str, out: TextIO):
     out.write("# " + state + "_" + module + "\n")
-    
-    module_folders = [os.path.join(os.environ['DIST_PATH'], "modules", module),
-                      os.path.join(os.environ['CUSTOM_PI_OS_PATH'], "modules", module)
-        ]
-    
-    found_local = False
-    found_remote = False
-    for module_folder in module_folders:
-        if os.path.isdir(module_folder):
-            found_local = True
-            break
-        
-    if not found_local:
-        # TODO: Handle update
-        found_remote, module_folder = get_remote_module(module)
-        
-        
-    if not found_local and not found_remote:
-        print(f"Error: Module {module} does not exist and is not in remote modules list")
-        exit(1)
-            
     script = os.path.join(module_folder, state + "_chroot_script")
     if os.path.isfile(script):
         out.write("execute_chroot_script '" + module_folder + "' '" + script + "'\n")
@@ -34,38 +44,95 @@ def handle(module, state, out):
     
     return
 
-def parse(a, callback):
+def parse(a: str) -> List[Tuple[str,str]]:
     stack=[]
+    return_value = []
     token = ""
     
     for char in a:
         if char == "(":
             stack.append(token)
             if token != "":
-                callback(token, "start")
+                return_value.append((token, "start"))
             token = ""
         elif char == ")":
             parent = stack.pop()
             if token != "":
-                callback(token, "start")
-                callback(token, "end")
+                return_value.append((token, "start"))
+                return_value.append((token, "end"))
                 token = ""
             if parent != "":
-                callback(parent, "end")
+                return_value.append((parent, "end"))
         elif char == ",":
             if token != "":
-                callback(token, "start")
-                callback(token, "end")
+                return_value.append((token, "start"))
+                return_value.append((token, "end"))
                 token = ""
         else:
             token += char
 
     if token != "":
-        callback(token, "start")
-        callback(token, "end")
+        return_value.append((token, "start"))
+        return_value.append((token, "end"))
     if len(stack) > 0:
         raise Exception(str(stack))
-    return
+    return return_value
+
+def handle_meta_modules(modules: List[Tuple[str,str]]) -> Tuple[List[Tuple[str,str]],Dict[str,str]]:
+    return_value = []
+    modules_to_modules_folder = {}
+    for module, state in modules:
+        module_folders = [
+            os.path.join(os.environ['DIST_PATH'], "modules", module),
+            os.path.join(os.environ['CUSTOM_PI_OS_PATH'], "modules", module)
+        ]
+        # In case this is a meta module, order counts
+        if state == "start":
+            return_value.append((module, state))
+        found_local = False
+        found_remote = False
+        for module_folder in module_folders:
+            if os.path.isdir(module_folder):
+                found_local = True
+                modules_to_modules_folder[module] = module_folder
+                break
+            
+        if not found_local:
+            # TODO: Handle update
+            found_remote, module_folder = get_remote_module(module)
+            modules_to_modules_folder[module] = module_folder
+            
+            
+        if not found_local and not found_remote:
+            print(f"Error: Module {module} does not exist and is not in remote modules list")
+            exit(1)
+
+        meta_module_path = os.path.join(module_folder, "meta")
+        if os.path.isfile(meta_module_path):
+            # Meta module detected
+            print(f"Running: {meta_module_path}")
+            print(f"ENV: {os.environ['BASE_BOARD']}")
+            submodules, meta_module_errors, is_timeout = run_command(meta_module_path)
+            submodules = submodules.strip()
+            print(f"Adding in modules: {submodules}")
+            if meta_module_errors != "" or is_timeout:
+                print(meta_module_errors)
+                print(f"Got error processing meta module at: {meta_module_path}")
+                exit(1)
+            if submodules != "":
+                print(f"Got sub modules: {submodules}")
+
+                for sub_module in submodules.split(","):
+                    sub_module = sub_module.strip()
+                    return_value_sub, modules_to_modules_folder_sub = handle_meta_modules([(sub_module, state)])
+                    return_value += return_value_sub
+                    modules_to_modules_folder.update(modules_to_modules_folder_sub)
+        # In case this is a meta module, order counts
+        if state == "end":
+            return_value.append((module, state))
+
+    return return_value, modules_to_modules_folder
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=True, description='Parse and run CustomPiOS chroot modules')
@@ -80,7 +147,14 @@ if __name__ == "__main__":
         f.write("#!/usr/bin/env bash\n")
         f.write("set -x\n")
         f.write("set -e\n")
-        parse(args.modules.replace(" ", ""), lambda module, state: handle(module, state, f))
+        initial_execution_order = parse(args.modules.replace(" ", ""))
+        f.write(f"# Defined execution order: {initial_execution_order}\n")
+        modules_execution_order, modules_to_modules_folder = handle_meta_modules(initial_execution_order)
+        f.write(f"# With meta modules order: {modules_execution_order}\n")
+        
+        for module, state in modules_execution_order:
+            module_folder = modules_to_modules_folder[module]
+            write_modules_scripts(module, state, module_folder, f)
         
     os.chmod(args.output_script, 0o755)
 
